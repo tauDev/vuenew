1537169f826dcec22db2c6e9c972687bccb0452a
Parinya <parinyat0926@gmail.com>
2018-05-24 15:34:32 +0700

    update

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,11 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import axios from 'axios'
 import HelloWorld from '@/components/HelloWorld'
 import Page1 from '@/components/Page1'
 import Page2 from '@/components/Page2'
 
-Vue.use(Router,axios)
+Vue.use(Router)
 
 export default new Router({
   routes: [
