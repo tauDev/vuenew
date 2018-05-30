1329d870a84d769e50e57cf49834caa816e5f6b4
Parinya <parinyat0926@gmail.com>
2018-05-30 17:59:48 +0700

    Update

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Page1 from '@/components/Page1'
 import Page2 from '@/components/Page2'
+import PageEdit from '@/components/PageEdit'
 
 Vue.use(Router)
 export default new Router({
@@ -24,6 +25,12 @@ export default new Router({
       name: 'Page2',
       component: Page2,
       props: true
-    }
+    },
+    {
+    path: '/edit/:id',
+    name: 'Edit',
+    component: PageEdit,
+    props: true
+  }
   ]
 })
